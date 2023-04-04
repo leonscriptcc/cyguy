2ce35d5e2d703a916a1b42fd178a5f5bc1b87e3a
李昂 <liang@duwitech.com>
2023-04-04 20:42:40 +0800

    feat(relationship): DELETE SET
    1、完成relationship的set和delete

--- cyguy/relationship.go
+++ cyguy/relationship.go
@@ -66,5 +66,60 @@ func (r *Relationship) Create() (result string, err error) {
 
 // Delete 删除关系
 func (r *Relationship) Delete() (result string, err error) {
+	if r.err != nil {
+		return result, err
+	}
+
+	// 判断节点信息
+	if r.fromNode == nil || r.toNode == nil {
+		err = errors.New("missing node information")
+		return result, err
+	}
+
+	// 组装语句
+	result = fmt.Sprintf("%s (%s)-[%s]-%s %s %s",
+		MATCH, r.fromNode.info, r.info, r.toNode.info, DELETE, r.name)
+
+	return result, r.err
+}
+
+// SetLabels 更新标签
+func (r *Relationship) SetLabels(labels string) (result string, err error) {
+	if r.err != nil {
+		return result, err
+	}
+
+	// 判断节点信息
+	if r.fromNode == nil || r.toNode == nil {
+		err = errors.New("missing node information")
+		return result, err
+	}
+
+	// 组装语句
+	result = fmt.Sprintf("%s (%s)-[%s]-%s %s %s",
+		MATCH, r.fromNode.info, r.info, r.toNode.info, DELETE, r.name)
+
+	return result, r.err
+}
+
+// SetProperties 更新属性
+func (r *Relationship) SetProperties(obj any) (result string, err error) {
+	if r.err != nil {
+		return result, err
+	}
+
+	// 判断节点信息
+	if r.fromNode == nil || r.toNode == nil {
+		err = errors.New("missing node information")
+		return result, err
+	}
+
+	// 获取等待更新的obj
+	ps, err := getProperties(obj)
+
+	// 组装语句
+	result = fmt.Sprintf("%s (%s)-[%s]-%s %s %s=%s",
+		MATCH, r.fromNode.info, r.info, r.toNode.info, SET, r.name, ps)
+
 	return result, r.err
 }
