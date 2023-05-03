3ccc52e7f36b5df3e9954a9ffc0f992cc4526ae5
李昂 <liang@duwitech.com>
2023-05-03 09:50:29 +0800

    feat(cyguy): MATCH
    1、完成多跳查询语句

--- match.go
+++ match.go
@@ -12,8 +12,11 @@ func (c *CypherGuy) Matcher() *Matcher {
 
 // Matcher 查询器
 type Matcher struct {
-	result []byte
-	err    error
+	result   []byte
+	err      error
+	fromNode *Node
+	toNode   *Node
+	r        *Relationship
 }
 
 // Nodes 根据node的属性查询节点
@@ -23,23 +26,46 @@ func (m *Matcher) Nodes(node *Node) {
 		MATCH, node.label, node.properties, RETURN))
 }
 
-// MultiJumps 多跳查询-设置节点
-// MATCH (n)-[:rel3]->(m) RETURN n
-func (m *Matcher) MultiJumps(fromNode *Node, relationship Relationship, toNode *Node) *Matcher {
-	if fromNode == nil && toNode == nil {
-		m.err = errors.New("")
-		return m
-	}
-
-	if fromNode == nil {
-
-	} else if toNode == nil {
-
-	}
-	return m
-}
-
 // Find 生成查询语句
 func (m *Matcher) Find() string {
 	return string(m.result)
+}
+
+func (m *Matcher) FromNode(node *Node) *Matcher {
+	m.fromNode = node
+	return m
+}
+
+func (m *Matcher) ToNode(relationship *Relationship, node *Node) *Matcher {
+	m.r = relationship
+	m.toNode = node
+	return m
+}
+
+// MultiJumps 多跳查询-设置节点
+// MATCH (n)-[:rel3*1..]->(m:label) RETURN n
+func (m *Matcher) MultiJumps() (result string, err error) {
+	// 关系不能为空
+	if m.r == nil {
+		err = errors.New("")
+		return "", err
+	}
+
+	// 不能同时为空
+	if m.fromNode == nil && m.toNode == nil {
+		err = errors.New("")
+		return "", err
+	}
+
+	// 组装查询语句
+	if m.fromNode == nil {
+		result = fmt.Sprintf("%s (n)-[r:%s*1..]->(m:%s) %s n,m",
+			MATCH, m.r.label, m.toNode.label, RETURN)
+	} else if m.toNode == nil {
+		result = fmt.Sprintf("%s (n:%s)-[r:%s*1..]->(m) %s n,m",
+			MATCH, m.r.fromNode.label, m.r.label, RETURN)
+	}
+
+	return result, nil
+
 }
